c5e639fb1cc1d5199694a5d4e15273a876fa96f4
Bladimir Lopez Almendras <b.lopez.almendras@gmail.com>
2024-10-23 17:17:16 -0400

    esto es una versión mejorada ya con notificacion

--- app/src/main/java/com/example/radio/MainActivity.java
+++ app/src/main/java/com/example/radio/MainActivity.java
@@ -13,146 +13,173 @@ import android.widget.Toast; // Mostrar mensajes breves en pantalla
 import android.telephony.PhoneStateListener; // Para escuchar cambios en el estado del teléfono
 import android.telephony.TelephonyManager; // Acceso al servicio de telefonía
 import androidx.appcompat.app.AppCompatActivity; // Clase base para actividades
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import androidx.core.app.NotificationCompat;
+
+import java.io.IOException;
 
 
-/** @noinspection ALL*/ // Definición de la actividad principal de la aplicación
+// MainActivity es la actividad principal de la aplicación
 public class MainActivity extends AppCompatActivity {
-    private MediaPlayer mediaPlayer; // Reproductor de audio
-    private ImageButton playPauseButton; // Botón de reproducir/pausar
-    private boolean isPlaying = false; // Estado de la reproducción (true si está reproduciendo)
+    private static final String CHANNEL_ID = "radio_channel"; // ID del canal de notificación
+    private static final int NOTIFICATION_ID = 1; // ID de la notificación
 
-    private AudioManager audioManager; // Control de audio del dispositivo
-    private SeekBar seekBarVolume; // Control deslizante para el volumen
-
-
+    private MediaPlayer mediaPlayer; // Objeto para manejar la reproducción de audio
+    private ImageButton playPauseButton; // Botón para reproducir o pausar la música
+    private boolean isPlaying = false; // Indica si la música está en reproducción
+    private AudioManager audioManager; // Administrador de audio del dispositivo
+    private SeekBar seekBarVolume; // Barra deslizante para controlar el volumen
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // Establece el diseño de la interfaz principal
+        setContentView(R.layout.activity_main); // Establece el diseño de la actividad principal
 
-        // Inicialización de botones y vistas
-        playPauseButton = findViewById(R.id.buttonPlayPause); // Botón de reproducir/pausar
-        View facebook = findViewById(R.id.facebook); // Vista para el enlace a Facebook
-        View whatsapp = findViewById(R.id.whatsapp); // Vista para el enlace a WhatsApp
-        View youtube = findViewById(R.id.youtube); // Vista para el enlace a YouTube
-        View pagina = findViewById(R.id.pagina); // Vista para el enlace a la página web
+        initializeUI();
+        initializeMediaPlayer();
+        initializeVolumeControl();
+        initializePhoneStateListener();
+        createNotificationChannel();
+    }
 
-        mediaPlayer = new MediaPlayer(); // Inicializa el reproductor de audio
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Radio Channel"; // Nombre del canal
+            String description = "Canal para notificaciones de la radio"; // Descripción del canal
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(description);
 
-        // Configuración de clics en los botones
-        playPauseButton.setOnClickListener(v -> togglePlayPause()); // Alterna entre reproducir y pausar
-        facebook.setOnClickListener(v -> openLink("https://www.facebook.com/profile.php?id=100077840239066")); // Abre enlace a Facebook
-        whatsapp.setOnClickListener(v -> openLink("https://twitter.com/tu_perfil")); // Abre enlace a WhatsApp
-        youtube.setOnClickListener(v -> openLink("https://www.instagram.com/tu_perfil")); // Abre enlace a YouTube
-        pagina.setOnClickListener(v -> openLink("https://tapacari.gob.bo/")); // Abre enlace a la página web
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel); // Crea el canal de notificación
+        }
+    }
 
-        // Inicializar el AudioManager para controlar el volumen del dispositivo
-        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE); // Obtiene el servicio de audio
+    private void showNotification(String title, String message) {
+        int icon = isPlaying ? R.drawable.pausa : R.drawable.play; // Selecciona el ícono según el estado de reproducción
 
-        // Configurar el SeekBar de volumen
-        ImageButton buttonVolume = findViewById(R.id.buttonVolume); // Botón para mostrar/ocultar el control de volumen
-        seekBarVolume = findViewById(R.id.seekBarVolume); // Barra deslizante para el volumen
-        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC); // Obtiene el volumen máximo
-        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC); // Obtiene el volumen actual
-        seekBarVolume.setMax(maxVolume); // Establece el volumen máximo en el SeekBar
-        seekBarVolume.setProgress(currentVolume); // Establece el volumen actual en el SeekBar
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(icon) // Ícono de la notificación
+                .setContentTitle(title) // Título de la notificación
+                .setContentText(message) // Mensaje de la notificación
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true); // Elimina la notificación al hacer clic
 
-        // Configura el clic en el botón de volumen para mostrar u ocultar el SeekBar
-        buttonVolume.setOnClickListener(v -> {
-            if (seekBarVolume.getVisibility() == View.GONE) {
-                seekBarVolume.setVisibility(View.VISIBLE); // Muestra el SeekBar
-                Toast.makeText(MainActivity.this, "Control de volumen visible", Toast.LENGTH_SHORT).show(); // Mensaje de notificación
-            } else {
-                seekBarVolume.setVisibility(View.GONE); // Oculta el SeekBar
-                Toast.makeText(MainActivity.this, "Control de volumen oculto", Toast.LENGTH_SHORT).show(); // Mensaje de notificación
-            }
-        });
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
 
-        // Configura el comportamiento del SeekBar cuando se cambia su valor
+    private void cancelNotification() {
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.cancel(NOTIFICATION_ID); // Cancela la notificación por ID
+    }
+
+    private void initializeUI() {
+        playPauseButton = findViewById(R.id.buttonPlayPause);
+        playPauseButton.setOnClickListener(v -> togglePlayPause());
+
+        // Configura los enlaces de las vistas a diferentes URLs
+        setOnClickListener(R.id.facebook, "https://www.facebook.com/profile.php?id=100077840239066");
+        setOnClickListener(R.id.whatsapp, "https://twitter.com/tu_perfil");
+        setOnClickListener(R.id.youtube, "https://www.instagram.com/tu_perfil");
+        setOnClickListener(R.id.pagina, "https://tapacari.gob.bo/");
+    }
+
+    private void setOnClickListener(int viewId, String url) {
+        findViewById(viewId).setOnClickListener(v -> openLink(url)); // Asigna el enlace a la vista especificada
+    }
+
+    private void initializeMediaPlayer() {
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setOnPreparedListener(mp -> mediaPlayer.start());
+    }
+
+    private void initializeVolumeControl() {
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        ImageButton buttonVolume = findViewById(R.id.buttonVolume);
+        seekBarVolume = findViewById(R.id.seekBarVolume);
+
+        seekBarVolume.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+        seekBarVolume.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
+
+        buttonVolume.setOnClickListener(v -> toggleSeekBarVisibility());
         seekBarVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 if (fromUser) {
-                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0); // Establece el volumen según el progreso del SeekBar
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0); // Cambia el volumen
                 }
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) { } // No se necesita implementar lógica al iniciar el seguimiento
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // Método vacío
+            }
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) { } // No se necesita implementar lógica al detener el seguimiento
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Método vacío
+            }
         });
+    }
 
-        // Configuración de TelephonyManager y PhoneStateListener para detectar cambios en el estado de las llamadas
-        // Servicio para monitorear el estado del teléfono
-        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE); // Obtiene el servicio de telefonía
-        // Llama a la implementación de la superclase
-        // Cuando hay una llamada activa
-        // Cuando el teléfono está sonando
-        // Pausa la reproducción si el teléfono está en llamada o suena
-        // Cuando no hay llamadas activas
-        // No hacer nada cuando la llamada termina
-        // Listener para detectar cambios en el estado de las llamadas
-        PhoneStateListener phoneStateListener = new PhoneStateListener() {
+    private void toggleSeekBarVisibility() {
+        int visibility = (seekBarVolume.getVisibility() == View.GONE) ? View.VISIBLE : View.GONE;
+        seekBarVolume.setVisibility(visibility); // Cambia la visibilidad del SeekBar
+        Toast.makeText(this, visibility == View.VISIBLE ? "Control de volumen visible" : "Control de volumen oculto", Toast.LENGTH_SHORT).show();
+    }
+
+    private void initializePhoneStateListener() {
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        telephonyManager.listen(new PhoneStateListener() {
             @Override
             public void onCallStateChanged(int state, String phoneNumber) {
-                super.onCallStateChanged(state, phoneNumber); // Llama a la implementación de la superclase
-                switch (state) {
-                    case TelephonyManager.CALL_STATE_OFFHOOK: // Cuando hay una llamada activa
-                    case TelephonyManager.CALL_STATE_RINGING: // Cuando el teléfono está sonando
-                        if (isPlaying) {
-                            togglePlayPause(); // Pausa la reproducción si el teléfono está en llamada o suena
-                        }
-                        break;
-                    case TelephonyManager.CALL_STATE_IDLE: // Cuando no hay llamadas activas
-                        // No hacer nada cuando la llamada termina
-                        break;
+                if ((state == TelephonyManager.CALL_STATE_OFFHOOK || state == TelephonyManager.CALL_STATE_RINGING) && isPlaying) {
+                    togglePlayPause(); // Pausa la reproducción
                 }
             }
-        };
-
-        // Registrar el listener para cambios en el estado del teléfono
-        telephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE); // Escucha cambios en el estado de la llamada
+        }, PhoneStateListener.LISTEN_CALL_STATE); // Registra el listener para cambios de estado
     }
 
-    private void openLink(String url) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(url)); // Establece el URI de la URL
-        startActivity(intent); // Inicia la actividad para abrir el enlace
-    }
-
-
-    // Método para reproducir o pausar el audio
     private void togglePlayPause() {
         if (isPlaying) {
-            stopRadio(); // Detiene la reproducción si está activa
-            playPauseButton.setImageResource(R.drawable.play); // Cambia el ícono a "play"
+            stopRadio();
+            updatePlayPauseButton(R.drawable.play, "Pausado", "Radio Tapacari");
         } else {
-            playRadio(); // Inicia la reproducción si está detenida
-            playPauseButton.setImageResource(R.drawable.pausa); // Cambia el ícono a "pause"
+            playRadio();
+            updatePlayPauseButton(R.drawable.pausa, "Reproduciendo", "Radio Tapacari");
+            Toast.makeText(this, "Reproduciendo", Toast.LENGTH_SHORT).show(); // Mensaje de reproducción
         }
         isPlaying = !isPlaying; // Alterna el estado de reproducción
     }
 
-    // Método para iniciar la reproducción de la radio
+    private void updatePlayPauseButton(int icon, String title, String message) {
+        playPauseButton.setImageResource(icon); // Cambia el ícono
+        showNotification(title, message); // Muestra la notificación
+    }
+
     private void playRadio() {
         try {
-            mediaPlayer.reset(); // Resetea el reproductor de audio
-            String RADIO_URL = "https://stream.zeno.fm/xtvmziqutm5vv"; // URL de la radio
-            mediaPlayer.setDataSource(RADIO_URL); // Establece la fuente de datos para el reproductor
-            mediaPlayer.prepareAsync(); // Prepara la reproducción de forma asíncrona
-            mediaPlayer.setOnPreparedListener(mp -> mediaPlayer.start()); // Inicia la reproducción cuando esté preparado
-        } catch (Exception e) {
-            e.printStackTrace(); // Imprime la traza de errores en caso de excepción
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource("https://stream.zeno.fm/xtvmziqutm5vv"); // URL de la transmisión de radio
+            mediaPlayer.prepareAsync();
+        } catch (IOException e) {
+            Toast.makeText(this, "Error al iniciar la transmisión", Toast.LENGTH_SHORT).show(); // Mensaje de error
         }
     }
 
-    // Método para detener la reproducción de la radio
     private void stopRadio() {
-        if (mediaPlayer.isPlaying()) { // Comprueba si el reproductor está en reproducción
-            mediaPlayer.stop(); // Detiene la reproducción
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            playPauseButton.setImageResource(R.drawable.stop); // Cambia el ícono a "Stop"
         }
     }
+
+    private void openLink(String url) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url))); // Crea una intención para abrir el enlace
+    }
 }
+
